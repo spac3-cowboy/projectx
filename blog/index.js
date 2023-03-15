1559360d7dc57ebf0b20cec40539f16961d5a643
spac3-cowboy <bhudipta15-5452@diu.edu.bd>
2023-03-15 22:17:44 +0600

    edge templating

--- blog/index.js
+++ blog/index.js
@@ -1,7 +1,32 @@
+const path = require('path');
+const expressEdge = require('express-edge');
 const express = require('express');
 const app = new express();
 
 app.use(express.static('public'));
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/pages/about.html');
+    });
+
+app.get('/contact', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    });
+    
+app.get('/post', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    });
+
 
 app.listen(4000, () => {
     console.log('App listening on port 4000!');
